30aad33bfa07feeaa2451a75dc001ddd412d09a0
Laura Green <LGreen@spartaglobal.com>
2018-12-10 18:34:55 +0000

    basic calculator complete and code tidied up with .Lowercase() function

---
+++ lab.js
@@ -0,0 +1,33 @@
+
+   var calculator = prompt ("hello, would you like to use a (b)asic or (a)dvanced calculator?").toLowerCase();
+    if (calculator == "b")
+    {var basicFunction = prompt("Would you like to use the (a)dd (d)ivide, (m)ultiply or (s)ubtract?").toLowerCase();
+    }
+      if (calculator == "b" && basicFunction == "d"){
+        var number = prompt("enter a number");
+        var divideBy = prompt("divide by")
+        alert(result = number / divideBy);
+      }
+      else if (calculator == "b" && basicFunction == "a"){
+        var numberToAdd = prompt("enter a number");
+        var nowAdd = prompt("Plus");
+        alert(result = parseInt(numberToAdd) + parseInt(nowAdd));
+      }
+      else if (calculator == "b" && basicFunction == "m"){
+        var numberToMultiply = prompt("enter a number");
+        var multiplyBy = prompt("Times by")
+        alert(result = numberToMultiply * multiplyBy);
+      }
+      else if (calculator == "b" && basicFunction == "s"){
+        var number = prompt("enter a number");
+        var takeAway = prompt("minus")
+        alert(result = number - takeAway);
+      }
+
+
+    // else if (calculator == "advanced" && advancedFunction == "Square     Root"){
+    //   var number = prompt("enter a number");
+    //   var divideBy = prompt("divide by")
+    //   alert(result = number  divideBy);
+    // var advanced-calculation =
+    // if (calculator == "advanced" && )
